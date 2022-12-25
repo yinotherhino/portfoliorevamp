7e2be1ac44112564bd7b4f22a8ba3d1ef17f2ccd
yinotherhino <muhammedbayero@gmail.com>
2022-12-25 08:20:26 +0100

    added socialicons

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './SocialLinks.module.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const SocialLinks = () => {
   return (
     <>
-        
+        <a href='https://github.com/yinotherhino'><GitHubIcon /></a>
+        <a href='https://github.com/yinotherhino'><FacebookIcon /></a>
+        <a href='https://github.com/yinotherhino'><TwitterIcon /></a>
+        <a href='https://github.com/yinotherhino'><LinkedInIcon /></a>
     </>
   )
 }
