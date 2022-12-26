0f279b90773ac8ae3a93cdacda5eec39170cb731
yinotherhino <muhammedbayero@gmail.com>
2022-12-26 03:36:12 +0100

    moved navbar to show on loading

--- src/components/Dp/Dp.tsx
+++ src/components/Dp/Dp.tsx
@@ -4,6 +4,11 @@ import dpimg from '../../assets/dp.png'
 import node from '../../assets/node.png'
 import django from '../../assets/django.png'
 import ts from '../../assets/typescript.png'
+import react from '../../assets/react.png'
+import docker from '../../assets/docker.png'
+import mongo from '../../assets/mongo.png'
+
+
 
 
 import Floatingimg from '../Floatingimg/Floatingimg'
@@ -13,10 +18,13 @@ const Dp = () => {
     <div className={styles.dpcontainer}>
 
         <div className={styles.rounded}>
-            <Floatingimg src={node} positioning={{topmargin:"50%", leftmargin:"-30%"}} />
+            <Floatingimg src={node} positioning={{topmargin:"40%", leftmargin:"-30%"}} />
             <Floatingimg src={django} positioning={{topmargin:"20%", leftmargin:"-30%"}} />
-
+            <Floatingimg src={docker} positioning={{topmargin:"20%", leftmargin:"100%"}} />
             <Floatingimg src={ts} positioning={{topmargin:"-5%", leftmargin:"-10%"}} />
+            <Floatingimg src={react} positioning={{topmargin:"40%", leftmargin:"110%"}} />
+            <Floatingimg src={mongo} positioning={{topmargin:"-5%", leftmargin:"80%"}} />
+
 
             <img src={dpimg} alt="muhammed bayero yinotherhino" className={styles.dpimg} />
         </div>
