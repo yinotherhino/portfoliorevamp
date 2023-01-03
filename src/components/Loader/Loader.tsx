9156e4dec7b30ce571dbe704d01af8c7cb5baed6
yinotherhino <muhammedbayero@gmail.com>
2023-01-03 10:15:17 +0100

    removed reacttpwrter and mui, added typewriter-effect

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import styles from './Loader.module.css'
-import TypeWriterEffect from 'react-typewriter-effect';
+import TypeWriterEffect from 'typewriter-effect';
 
 const Loader = () => {
   const welcomeTxt = 'Welcome to my portfolio page';
 
   return (
     <div className={styles.loadcontainer}>
-    <TypeWriterEffect
-      startDelay={100}
-      cursorColor="black"
-      text={welcomeTxt}
-      typeSpeed={50}
-      eraseSpeed={50}
-    />
+        <TypeWriterEffect
+          onInit={(typewriter) => {
+            typewriter.typeString(welcomeTxt)
+              .callFunction(() => {
+                console.log('String typed out!');
+              })
+              .start();
+          }}
+/>
     <br />
 
     <div className={styles.loader}> </div>
